1d74c89041c0956786b64d715075b2d03882a428
hosomi-hayato <101035362+hosomi-hayato@users.noreply.github.com>
2022-03-06 16:01:29 +0900

    Add files via upload

---
+++ header.php
@@ -0,0 +1,12 @@
+<br>
+  <div class="boxa">
+   <div class="boxb">
+     <p>〇△☓学校</p>
+   </div>
+   <div class="boxc">
+     <a href="fast.php">はじめに</a>
+   </div>
+   <div class="boxd">
+     <a href="use.php">使い方</a>
+   </div>
+</div>
